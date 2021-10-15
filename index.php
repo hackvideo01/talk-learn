36a7b4c97ffef9fc4c2ed8c4fa7bb5b43381565a
hackvideo01 <hackvideo01@gmail.com>
2021-10-15 18:45:29 +0900

    u2

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?
+ini_set('display_errors', 'Off');
 include_once("header.php");
-include_once("index.html");
+
+
+$id = htmlspecialchars($_REQUEST['id']);
+
+
+if ($id==null) {
+	include_once("index.html");
+}else if ($id==1) {
+	include_once("talk-learn.php");
+}
+
 include_once("footer.php");
 
 
